34411497627a22b3f0f2149547e0cded26e4373c
Liangxujian <13692505204@163.com>
2018-10-18 11:59:34 +0800

    idea git test

--- src/com/mmc/test/Test.java
+++ src/com/mmc/test/Test.java
@@ -4,7 +4,11 @@ package com.mmc.test;
  * 可操作测试类
  */
 public class Test {
-	public static void main(String[] args) {
-		String str = "hello/";
-	}
+    public static void main(String[] args) {
+        String str = "hello/";
+    }
+
+    public void run(){
+        System.out.println("我跑得可快了");
+    }
 }
